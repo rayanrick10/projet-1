b71be5f05a34c603dfd3b6f8f3937097504d044f
rayanrick10 <rayanrick10@gmail.com>
2023-08-07 18:27:12 +0100

    rayan

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    effect: "fade",
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+  
