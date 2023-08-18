25bfa77dccf38bcaa85afc9dbc6fa3c0e7f98d37
rayanrick10 <rayanrick10@gmail.com>
2023-08-18 12:08:05 +0100

    flor

--- js/rick.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
